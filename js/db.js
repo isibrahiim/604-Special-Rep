0f322789b8b626fc9e0857364cd26dc3474e43a1
isse salad <you@example.com>
2021-06-25 16:42:31 +0300

    initial commit

---
+++ js/db.js
@@ -0,0 +1,28 @@
+// Display Time function
+function updateClock() {
+    document.querySelector(".clock").style.color = "#fff";
+    const clockContainer = document.querySelector(".clock");
+  
+    clockContainer.innerHTML =
+      // "<b>" + new Date().toLocaleDateString("uk") + "</b>";
+      "<b>" + new Date().toLocaleTimeString("uk") + "</b>";
+  }
+  setInterval(updateClock, 1000);
+  // JSON Data to HTML 
+  const bio = {
+    "title": "What I do",
+    "btm-line" : '',
+    "desc": " I'm a Web designer and also a Designer, I work in the illustration niche, as well as graphic and motion design, photography, and combine them with other visual arts. I work with commercial and social projects of any scale and bring every work to the highest level of perfection.",
+    
+  };
+  
+  let bioHtml = "";
+  bioHtml += `
+  
+  <h2 class="section-title"> ${bio.title} </h2>
+  <div class="bottom-line"> </div>
+  <p class = 'lead'> ${bio.desc}</p>
+  `;
+  document.querySelector(".bio").innerHTML = bioHtml;
+  
+  
